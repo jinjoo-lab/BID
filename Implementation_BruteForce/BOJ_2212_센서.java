dfb6fded99cdb37b4ac13e431c5a4a4219dcb965
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-04-20 20:29:17 +0900

    [BI] BOJ 2212 센서

---
+++ Implementation_BruteForce/BOJ_2212_센서.java
@@ -0,0 +1,55 @@
+package Implementation_BruteForce;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_2212_센서 {
+    static int n,k;
+    static int[] board;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        n = Integer.parseInt(st.nextToken());
+        board = new int[n];
+
+        st = new StringTokenizer(br.readLine()," ");
+        k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0 ; i < n ; i++){
+            board[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(board);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (x,y) -> y - x
+        );
+
+        for(int i = 1  ; i < n ; i++){
+            pq.add(board[i] - board[i-1]);
+        }
+
+        if(k >= n){
+            System.out.println(0);
+            return;
+        }
+
+        long result = 0;
+        int idx = 0;
+        while(!pq.isEmpty()){
+            int cur = pq.poll();
+
+            if(idx < k - 1) {
+                idx = idx + 1;
+                continue;
+            }
+
+            result += cur;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
