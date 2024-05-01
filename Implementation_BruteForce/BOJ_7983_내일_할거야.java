ec325ed5f4c03c92edfd1a889a114def468f9052
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-01 13:43:33 +0900

    [BID] BOJ 7983 내일 할거야

---
+++ Implementation_BruteForce/BOJ_7983_내일_할거야.java
@@ -0,0 +1,51 @@
+package Implementation_BruteForce;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ_7983_내일_할거야 {
+
+    static int n;
+    static int[][] board;
+
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (x,y) -> {
+                if(x[1] == y[1])
+                    return y[0] - x[0];
+
+                return y[1] - x[1];
+            }
+    );
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+
+            int d = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            pq.add(new int[]{d,e});
+        }
+
+
+
+        int day = 1000_000_001;
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+
+            if(day > cur[1]){
+                day = cur[1];
+            }
+            day = day - cur[0];
+        }
+
+        System.out.println(day);
+
+        br.close();
+    }
+}
